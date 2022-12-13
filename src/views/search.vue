28c14706ac957a8f27636d599733b3ec6694ddc9
xrswyea <cjy1943222010@qq.com>
2022-12-13 23:02:03 +0800

    1

--- src/views/search.vue
+++ src/views/search.vue
@@ -4,13 +4,19 @@
         {{ router.params }}
         <hr />
         {{ router.query }}
+        <hr />
+        <button @click="send">click</button>
     </div>
 </template>
 
 <script setup lang="ts">
     import { useRoute } from "vue-router";
+    import requests from "@/api/request";
 
     const router = useRoute();
+    function send() {
+        requests.get("http://baidu.com");
+    }
 </script>
 
 <style scoped lang="less"></style>
